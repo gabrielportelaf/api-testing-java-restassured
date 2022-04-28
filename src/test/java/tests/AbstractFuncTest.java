cec4310480c8a18fc6ed96a30b131f2ed0aed5c7
Your Name <you@example.com>
2022-04-28 16:19:41 -0300

    Inclusão do .gitignore

--- src/test/java/tests/AbstractFuncTest.java
+++ src/test/java/tests/AbstractFuncTest.java
@@ -1,9 +1,7 @@
 package tests;
 
 public abstract class AbstractFuncTest {
-
     protected static String baseUrl = "https://petstore.swagger.io";
     protected static String version = "/v2";
     protected static String api = "/store";
-
 }
